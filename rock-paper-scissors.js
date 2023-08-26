8b0fa49c262f7b8a8a8015eff46af9b17c9e76a4
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2023-08-26 18:45:51 -0400

    Combine functions under one loop
    Originally I tried to separate functions for retrieving the player's
    selection, computer's selection, and the single-round game result.
    Although this allowed the correct player and computer selections, it
    did not return the correct comparisons to determine the winner. I kept
    the computer function separate, but combined player selection and game
    result under the same forEach loop.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,127 +1,99 @@
 
 
-        // Setup for recording the score and playing five rounds---------------------------------------------------
+// Setup for recording the score and playing five rounds---------------------------------------------------
 
-        let playerScore = 0;
-        let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-        function game() {
-            for(let i = 1; i <= 5; i++) { // This keeps the game running for five rounds
+function game() {
             
-            // Setup for stating the round being played
-            function Round() {
-                return i;
+// Setup for the player's selection of rock, paper, scissors------------------------------------------------------
+
+    const buttons = document.querySelectorAll('button');
+    const main = document.querySelector('.main');
+    const results = document.querySelector('.results');
+    const win = document.getElementById('win');
+    const player = document.getElementById('player');
+    const randomNumber = Math.random();
+    const computer = document.getElementById('computer');
+    const rock = document.getElementById('rock');
+    const paper = document.getElementById('paper');
+    const scissors = document.getElementById('scissors');
+    
+    const computerSelection = getComputerChoice();
+
+    //const computerTimeout = setTimeout(, 5000);
+
+    //-----------------------Computer Selection-----------------------
+
+    function getComputerChoice() {
+        let option;
+        if (randomNumber <= 0.35) {
+            let option = "rock";
+            return option;
+        } else if (randomNumber > 0.35 && randomNumber <= 0.65) {
+            let option = "paper";
+            return option;                   
+        } else if (randomNumber > 0.65) { 
+            let option = "scissors";
+            return option;
+        } else {
+            return "ERROR!";                     
+        }
+    }
+    
+    //-----------------------Player Selection-----------------------
+
+    function getPlayerChoice(event) {
+        let button = event.target;
+        return button.id;
+    }   
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            const playerSelection = getPlayerChoice(event);
+            player.textContent = "Player chose " + playerSelection;
+            computer.textContent = "Computer selected " + computerSelection;
+
+            if (playerSelection === computerSelection) {
+                win.textContent = "It's a draw";
+            } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+                win.textContent = "Player wins!";
+            } else {
+                win.textContent = "Computer wins!";
             }
-            alert("Playing ROUND: " + i);
+        });
+    }); 
 
-            console.log("Round: " + Round());
+    //-----------------------Round Winner-----------------------
 
-            // Setup for the player's selection of rock, paper, scissors------------------------------------------------------
-        
-            let playerChoice;
-        
+    
 
-            while (true) { // Instead of using a recursive soluntion involving the if condition as I tried before, I switched to an iterative approach to loop the prompt until the player chooses one of the correct options
-                playerChoice = prompt("ROCK, PAPER, SCISSORS ---which do you choose?");
-                const playerSelection = playerChoice.toLowerCase();
-                if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-                    break;
-                }
-                alert("Rock, paper, or scissors wasn't selected. Try again");
-            }
-            alert ("Player has selected " + (playerSelection = playerChoice.toLowerCase())); // I needed to redefine playerSelection because it was only defined in the while loop
+    //-----------------------Disable Buttons after click-----------------------
+    
+    function disableAllButtons () {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+    }
 
-            // The iterative approach allowed the code to record the correct value in console.log as oppose to the previous method
+    rock.addEventListener('click', () => {
+        disableAllButtons();
+    });
 
-            console.log("playerSelection: " + playerSelection);
+    paper.addEventListener('click', () => {
+        disableAllButtons();
+    });
 
-        
-            // Setup for the computer's selection of rock, paper, scissors------------------------------------------------------
+    scissors.addEventListener('click', () => {
+        disableAllButtons();
+    });
 
-            const computerSelection = getComputerChoice();
+    main.appendChild(player);
+    main.appendChild(computer);
+    results.appendChild(win);
+}
 
-            function getComputerChoice() {
-                let option;
-                const randomNumber = Math.random(); // Originally I defined this variable in the global scope, but decided to move it inside the local scope to avoid typing "randomNumber" inside the parentheses of "getComputerChoice()"
+game();
 
-                if (randomNumber <= 0.35) {
-                    let option = "rock";
-                    return option;
-                } else if (randomNumber > 0.35 && randomNumber <= 0.65) {
-                    let option = "paper";
-                    return option;
-                } else if (randomNumber > 0.65) { //originally I did not include this condition and instead simply used "else", but it caused issues
-                    let option = "scissors";
-                    return option;
-                } else {
-                    return "ERROR!"; // I included this to highlight whenever something other than the above conditions would have been incorrected labeled "SCISSORS" if the above condition was still "else"
-                }
-            }
-            alert("Computer selected " + computerSelection); // This is outside the function because I want an alert to notify the computerSelection
 
-            console.log("computerSelection: " + computerSelection);
-
-            // Setup for comparing selections to determine the outcome of the round------------------------------------------------
-
-            const result = playRound(playerSelection, computerSelection);
-
-            function playRound(playerSelection, computerSelection) {
-                if (playerSelection === computerSelection) {
-                    return "It's a draw";
-                } else if 
-                    ((playerSelection === "rock" && computerSelection === "scissors") || 
-                    (playerSelection === "paper" && computerSelection === "rock")|| 
-                    (playerSelection === "scissors" && computerSelection === "paper")) {
-                        return "Player wins the round!";
-                    } else if 
-                    ((playerSelection === "scissors" && computerSelection === "rock") || 
-                    (playerSelection === "rock" && computerSelection === "paper") || 
-                    (playerSelection === "paper" && computerSelection === "scissors")) {
-                        return "Computer wins the round!";
-                    } else {
-                    return "ERROR: wrong comparison result"; // Since I've already defined a tie condition, I replaced this with an error message in case there is an error in the code somewhere
-                }
-            }  
-        
-            alert("This round's result is: " + result);
-
-            console.log(result);
-            
-            // Setup for the score tracker----------------------------------------------------------------------
-
-            while (true) {
-                if (result === "Player wins the round!") {
-                    playerScore++;
-                    break;
-                } else if (result === "Computer wins the round!") {
-                    computerScore++;
-                    break;
-                } else {
-                    alert("No round winner");
-                    break;
-                }
-            }
-            alert("Player: " + playerScore + " | Computer: " + computerScore);
-
-            console.log("playerScore: " + playerScore + ", computerScore: " + computerScore);
-
-            } // End of the for loop (five rounds)
-
-            // I needed to move the winner() function, alert(winner()), and console.log(winner()) outside the loop so that it only runs at the end of all five rounds
-            function winner() {
-                if (playerScore > computerScore) {
-                    return "CONGRATULATIONS! PLAYER WINS!";
-                } else if (playerScore < computerScore) {
-                    return "Computer wins the game.";
-                } else {
-                    return "Sorry, there is no winner.";
-                }
-            } 
-            
-            alert(winner());
-
-            console.log(winner());
-
-        } // End of the game() function
-
-        console.log(game());

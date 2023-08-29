4390ad63126fc9b724924b076d1d3c0ed0fdf772
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2023-08-29 15:09:53 -0400

    Orangize functions within forEach loop
    Moved the function getComputerChoice inside the forEach loop to make the
    code more organized. Also moved the forEach loop inside a for loop such
    that (let i = 1; i <= 5; i++), which is supppose to run the game for
    five rounds. Instead, after one of the playable buttons was clicke, the
    game ran through 5 rounds with the same result. Another issue: the score
    is not properly recorded.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,8 +10,10 @@ function game() {
 // Setup for the player's selection of rock, paper, scissors------------------------------------------------------
 
     const buttons = document.querySelectorAll('button');
+    const roundNumber = document.getElementById('roundNumber');
     const main = document.querySelector('.main');
     const results = document.querySelector('.results');
+    const score = document.getElementById('score');
     const win = document.getElementById('win');
     const player = document.getElementById('player');
     const randomNumber = Math.random();
@@ -19,77 +21,109 @@ function game() {
     const rock = document.getElementById('rock');
     const paper = document.getElementById('paper');
     const scissors = document.getElementById('scissors');
-    
-    const computerSelection = getComputerChoice();
 
     //const computerTimeout = setTimeout(, 5000);
 
-    //-----------------------Computer Selection-----------------------
+    // I moved getComputerChoice() inside the forEach loop to keep things organized. Game functions the same.
 
-    function getComputerChoice() {
-        let option;
-        if (randomNumber <= 0.35) {
-            let option = "rock";
-            return option;
-        } else if (randomNumber > 0.35 && randomNumber <= 0.65) {
-            let option = "paper";
-            return option;                   
-        } else if (randomNumber > 0.65) { 
-            let option = "scissors";
-            return option;
-        } else {
-            return "ERROR!";                     
-        }
-    }
     
-    //-----------------------Player Selection-----------------------
 
     function getPlayerChoice(event) {
         let button = event.target;
         return button.id;
     }   
 
-    buttons.forEach((button) => {
-        button.addEventListener('click', (event) => {
-            const playerSelection = getPlayerChoice(event);
-            player.textContent = "Player chose " + playerSelection;
-            computer.textContent = "Computer selected " + computerSelection;
-            win.style = 'background: aqua; border: 10px double navy';
+    for (let i = 1; i <=5; i++) {
+        buttons.forEach((button) => {
+            button.addEventListener('click', (event) => {
+    
+                roundNumber.textContent = "Round " + i; 
+                roundNumber.style = 'border-bottom: 5px solid white;'
+                console.log(roundNumber.textContent);
 
-            function determineWinner() {
-                if (playerSelection === computerSelection) {
-                    win.textContent = "IT'S A DRAW -- NO WINNER";
-                } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-                    win.textContent = "PLAYER WINS!";
-                } else {
-                    win.textContent = "COMPUTER WINS!";
+                const playerSelection = getPlayerChoice(event);
+                player.textContent = "Player chose " + playerSelection;
+                console.log(player.textContent);
+
+                function getComputerChoice() {
+                    let option;
+                    if (randomNumber <= 0.35) {
+                        let option = "rock";
+                        return option;
+                    } else if (randomNumber > 0.35 && randomNumber <= 0.65) {
+                        let option = "paper";
+                        return option;                   
+                    } else if (randomNumber > 0.65) { 
+                        let option = "scissors";
+                        return option;
+                    } else {
+                        return "ERROR!";                     
+                    }
                 }
-            }
+                const computerSelection = getComputerChoice();
+                computer.textContent = "Computer selected " + computerSelection;
+                console.log(computer.textContent);
+    
+                const roundScore = roundWinner(playerSelection, computerSelection);
+    
+                function roundWinner(playerSelection, computerSelection) {
+                    if (playerSelection === computerSelection) {
+                        return "IT'S A DRAW -- NO WINNER";
+                    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+                        return "PLAYER WINS!";
+                    } else {
+                        return "COMPUTER WINS!";
+                    }
+                }
 
-            determineWinner();
+                win.style = 'background: aqua; border: 10px double navy';
+                win.textContent = roundScore;
+                console.log(roundScore);
+    
+                roundWinner();
+    
+                function countScore(roundScore) {
+                    while (true) {
+                        if (roundScore === "PLAYER WINS!") {
+                            playerScore++;
+                        } else if (roundScore === "COMPUTER WINS!") {
+                            computerScore++;
+                        } else {
+                            break;
+                        }
+                    }
+                    score.textContent = "Player: " + playerScore + " | Computer: " + computerScore;
+                }
+    
+                console.log(score.textContent);
+                countScore();
+    
+                function startAgain() {
+                    const playAgain = document.createElement('button');
+                    playAgain.textContent = 'Play Again';
+                    playAgain.style = 'background: royalblue; color: white; margin-top: 20px';
+                    results.appendChild(playAgain);
+    
+                    playAgain.addEventListener('click', (event) => {
+                        enableAllButtons();
+                        roundNumber.textContent = '';
+                        roundNumber.style = 'border-bottom: none';
+                        player.textContent = '';
+                        computer.textContent = '';
+                        win.textContent = '';
+                        win.style = 'background: navy; border: none';
+                        playAgain.remove();
+                    })
+                }
+    
+                startAgain();
+                
+            });
+        }); 
+    
+    }
 
-            function startAgain() {
-                const playAgain = document.createElement('button');
-                playAgain.textContent = 'Play Again';
-                results.appendChild(playAgain);
-
-                playAgain.addEventListener('click', (event) => {
-                    // Once you're able to clear the 'Play Again' button for each round, commit the success
-                    enableAllButtons();
-
-                    player.textContent = '';
-                    computer.textContent = '';
-                    win.textContent = '';
-                    win.style = 'background: navy; border: none';
-                    playAgain.remove();
-                })
-            }
-
-            startAgain();
-            
-        });
-    }); 
-
+    
     
 
     //-----------------------Disable Buttons after click-----------------------
@@ -118,9 +152,12 @@ function game() {
         scissors.disabled = false;
     }
 
+    
     main.appendChild(player);
     main.appendChild(computer);
+    results.appendChild(score);
     results.appendChild(win);
+    
 }
 
 game();

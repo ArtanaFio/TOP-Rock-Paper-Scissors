78b7c5ac73af3d2ade7d39c52e05781192207277
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2023-08-30 17:26:52 -0400

    Fix scoring issues and make style changes
    Removed the function countScore() and instead included playerScore++ and
    computerScore++ to the function roundWinner() such that a point is added
    to the correct variable when either player or computer wins a point.
    Able to display the round number after declaring new numberRound
    variable and including numberRound++ to the for Each loop
    addEventListerner. Also rearranged HTML elements and updated CSS to make
    the webpage more presentable. Now trying to debug code that is suppose
    to declare game winner after either opponent wins 5 points.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,18 +2,20 @@
 
 // Setup for recording the score and playing five rounds---------------------------------------------------
 
-let playerScore = 0;
-let computerScore = 0;
-
 function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+    let numberRound = 0;
             
-// Setup for the player's selection of rock, paper, scissors------------------------------------------------------
+    // Setup for the player's selection of rock, paper, scissors------------------------------------------------------
 
     const buttons = document.querySelectorAll('button');
     const roundNumber = document.getElementById('roundNumber');
     const main = document.querySelector('.main');
     const results = document.querySelector('.results');
     const score = document.getElementById('score');
+    const outcome = document.getElementById('outcome');
     const win = document.getElementById('win');
     const player = document.getElementById('player');
     const randomNumber = Math.random();
@@ -22,144 +24,142 @@ function game() {
     const paper = document.getElementById('paper');
     const scissors = document.getElementById('scissors');
 
-    //const computerTimeout = setTimeout(, 5000);
 
-    // I moved getComputerChoice() inside the forEach loop to keep things organized. Game functions the same.
+    //win.style = 'background: aqua; border: 10px double navy';
+
 
     
 
-    function getPlayerChoice(event) {
-        let button = event.target;
-        return button.id;
-    }   
+        function getPlayerChoice(event) {
+            let button = event.target;
+            return button.id;
+        }   
 
-    for (let i = 1; i <=5; i++) {
-        buttons.forEach((button) => {
-            button.addEventListener('click', (event) => {
-    
-                roundNumber.textContent = "Round " + i; 
-                roundNumber.style = 'border-bottom: 5px solid white;'
-                console.log(roundNumber.textContent);
+        
+            buttons.forEach((button) => {
+                button.addEventListener('click', (event) => {
+                    numberRound++;
+                    roundNumber.textContent = "Round " + numberRound; 
+                    roundNumber.style = 'border-bottom: 5px solid white';
+                    console.log(roundNumber.textContent);
 
-                const playerSelection = getPlayerChoice(event);
-                player.textContent = "Player chose " + playerSelection;
-                console.log(player.textContent);
+                    const playerSelection = getPlayerChoice(event);
+                    player.textContent = "Player chose " + playerSelection;
+                    console.log(player.textContent);
 
-                function getComputerChoice() {
-                    let option;
-                    if (randomNumber <= 0.35) {
-                        let option = "rock";
-                        return option;
-                    } else if (randomNumber > 0.35 && randomNumber <= 0.65) {
-                        let option = "paper";
-                        return option;                   
-                    } else if (randomNumber > 0.65) { 
-                        let option = "scissors";
-                        return option;
-                    } else {
-                        return "ERROR!";                     
-                    }
-                }
-                const computerSelection = getComputerChoice();
-                computer.textContent = "Computer selected " + computerSelection;
-                console.log(computer.textContent);
-    
-                const roundScore = roundWinner(playerSelection, computerSelection);
-    
-                function roundWinner(playerSelection, computerSelection) {
-                    if (playerSelection === computerSelection) {
-                        return "IT'S A DRAW -- NO WINNER";
-                    } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-                        return "PLAYER WINS!";
-                    } else {
-                        return "COMPUTER WINS!";
-                    }
-                }
-
-                win.style = 'background: aqua; border: 10px double navy';
-                win.textContent = roundScore;
-                console.log(roundScore);
-    
-                roundWinner();
-    
-                function countScore(roundScore) {
-                    while (true) {
-                        if (roundScore === "PLAYER WINS!") {
-                            playerScore++;
-                        } else if (roundScore === "COMPUTER WINS!") {
-                            computerScore++;
+                    function getComputerChoice() {
+                        let option;
+                        if (randomNumber <= 0.35) {
+                            let option = "rock";
+                            return option;
+                        } else if (randomNumber > 0.35 && randomNumber <= 0.65) {
+                            let option = "paper";
+                            return option;                   
+                        } else if (randomNumber > 0.65) { 
+                            let option = "scissors";
+                            return option;
                         } else {
-                            break;
+                            return "ERROR!";                     
                         }
                     }
+                    const computerSelection = getComputerChoice();
+                    console.log(randomNumber);
+                    computer.textContent = "Computer selected " + computerSelection;
+                    console.log(computer.textContent);
+        
+                    const roundScore = roundWinner(playerSelection, computerSelection);
+        
+                    function roundWinner(playerSelection, computerSelection) {
+                        if (playerSelection === computerSelection) {
+                            return "No points";
+                        } else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+                            playerScore++;
+                            return "Player wins the point";
+                        } else {
+                            computerScore++;
+                            return "Computer wins the point";
+                        }
+                    }
+
+
+                    results.style = "border-top: 5px solid white";
+                    outcome.textContent = roundScore;
+                    console.log(roundScore);
+
                     score.textContent = "Player: " + playerScore + " | Computer: " + computerScore;
-                }
-    
-                console.log(score.textContent);
-                countScore();
-    
-                function startAgain() {
-                    const playAgain = document.createElement('button');
-                    playAgain.textContent = 'Play Again';
-                    playAgain.style = 'background: royalblue; color: white; margin-top: 20px';
-                    results.appendChild(playAgain);
-    
-                    playAgain.addEventListener('click', (event) => {
-                        enableAllButtons();
-                        roundNumber.textContent = '';
-                        roundNumber.style = 'border-bottom: none';
-                        player.textContent = '';
-                        computer.textContent = '';
-                        win.textContent = '';
-                        win.style = 'background: navy; border: none';
-                        playAgain.remove();
-                    })
-                }
-    
-                startAgain();
-                
-            });
-        }); 
-    
-    }
+                    console.log(score.textContent);
+        
+                    roundWinner();
+                    
+                    function startAgain() {
+                        const playAgain = document.createElement('button');
+                        playAgain.textContent = 'Play Again';
+                        playAgain.style = 'background: royalblue; color: white; width: 250px; margin-top: 20px';
+                        main.appendChild(playAgain);
+            
+                        playAgain.addEventListener('click', (event) => {
+                            enableAllButtons();
+                            roundNumber.textContent = '';
+                            roundNumber.style = 'border-bottom: none';
+                            player.textContent = '';
+                            computer.textContent = '';
+                            outcome.textContent = '';
+                            win.textContent = '';
+                            playAgain.remove();
+                        })
+                    }
+            
+                    startAgain();
+                    
+                    function winner(playerScore, computerScore) {
+                        if (playerScore === 5) {
+                            win.textContent = "CONGRATULATIONS! YOU WIN!"
+                            disableAllButtons();
+                        } else if (computerScore === 5) {
+                            win.textContent = "Sorry, Computer wins";
+                            disableAllButtons();
+                        }
+                    }
 
+                    winner();
+                    
+                });
+            }); 
+        
+
+        //-----------------------Disable Buttons after click-----------------------
+        
+        function disableAllButtons() {
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
+        }
+
+        rock.addEventListener('click', () => {
+            disableAllButtons();
+        });
+
+        paper.addEventListener('click', () => {
+            disableAllButtons();
+        });
+
+        scissors.addEventListener('click', () => {
+            disableAllButtons();
+        });
+
+        function enableAllButtons() {
+            rock.disabled = false;
+            paper.disabled = false;
+            scissors.disabled = false;
+        }
+
+        
+        main.appendChild(player);
+        main.appendChild(computer);
+        main.appendChild(outcome);
+        results.appendChild(score);
+        results.appendChild(win);
     
-    
 
-    //-----------------------Disable Buttons after click-----------------------
-    
-    function disableAllButtons() {
-        rock.disabled = true;
-        paper.disabled = true;
-        scissors.disabled = true;
-    }
-
-    rock.addEventListener('click', () => {
-        disableAllButtons();
-    });
-
-    paper.addEventListener('click', () => {
-        disableAllButtons();
-    });
-
-    scissors.addEventListener('click', () => {
-        disableAllButtons();
-    });
-
-    function enableAllButtons() {
-        rock.disabled = false;
-        paper.disabled = false;
-        scissors.disabled = false;
-    }
-
-    
-    main.appendChild(player);
-    main.appendChild(computer);
-    results.appendChild(score);
-    results.appendChild(win);
-    
 }
-
 game();
-
-
